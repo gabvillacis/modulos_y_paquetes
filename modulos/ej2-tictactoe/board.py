5bcfac81f7339f445e82d92596e189b31ea7e18d
Gabriel Villacis <gab_villacis@hotmail.com>
2022-06-07 07:31:34 -0500

    Update tic-tac-toe game

--- modulos/ej2-tictactoe/board.py
+++ modulos/ej2-tictactoe/board.py
@@ -1,41 +1,50 @@
-from player import Player
-
 class Board:
-    def __init__(self):
-        self.board_status = {   'c1': '', 'c2': '', 'c3': '',
-                                'c4': '', 'c5': '', 'c6': '',
-                                'c7': '', 'c8': '', 'c9': '',
-                            }
+
+    def __init__(self, player_1, player_2):
+
+        self.board_status = { 'c1': '', 'c2': '', 'c3': '', 'c4': '', 'c5': '', 'c6': '', 'c7': '', 'c8': '', 'c9': '' }
 
         self.winning_conditions = ( ('c1', 'c2', 'c3'), ('c4', 'c5', 'c6'), ('c7', 'c8', 'c9'),
                                     ('c1', 'c4', 'c7'), ('c2', 'c5', 'c8'), ('c3', 'c6', 'c9'),
                                     ('c1', 'c5', 'c9'), ('c3', 'c5', 'c7'))
 
-        self.player_1 = Player('X')
-        self.player_2 = Player('O')
+        self.player_1 = player_1 
+        self.player_2 = player_2
+        self.current_player = player_1
 
-        self.current_player = self.player_1
+
+    def reset_board(self):
+        # Reiniciar el tablero a su estado inicial (vacío).
+
+        self.board_status = { 'c1': '', 'c2': '', 'c3': '', 'c4': '', 'c5': '', 'c6': '', 'c7': '', 'c8': '', 'c9': '' }
 
 
     def render_instructions(self):
-        # Mostrar las instrucciones del juego
+        # Mostrar las instrucciones del juego.
 
         print(' -  TIC TAC TOE  - ')
         print('\n')
         print('  c1  |  c2  |  c3  \n------+------+------\n  c4  |  c5  |  c6  \n------+------+------\n  c7  |  c8  |  c9  ')
         print('\n')
-        print('Jugador 1: X - Jugador 2: O')
-        print('En cada turno el jugador deberá insertar la celda que va utilizar.')
+        print(f'Jugador 1: {self.player_1} - Jugador 2: {self.player_2}')
+        print('En cada turno el jugador deberá ingresar la celda que va utilizar.')
 
 
     def render_board_status(self):
-        # Motrar el tablero con los valores de cada celda
+        # Mostrar el tablero con el valor actual de cada celda.
 
-        print('   {}   |   {}   |   {}   \n------+------+------\n   {}   |   {}   |   {}   \n------+------+------\n   {}   |   {}   |   {}   '.format(*self.board_status.values()))
+        cell_values =  [' ' if value == '' else value for value in self.board_status.values()]
+        print('  {}  |  {}  |  {}  \n-----+-----+-----\n  {}  |  {}  |   {}   \n-----+-----+-----\n  {}  |  {}  |  {}  '.format(*cell_values))
+
+    
+    def fill_cell(self, cell):
+        # Almacenar dentro de la celda seleccionada el jugador al cual pertenece.
+
+        self.board_status[cell] = self.current_player.id
 
     
     def check_board(self):
-        # Verificar el tablero para determinar si existe un ganador
+        # Verificar el tablero para determinar si existe un ganador.
         # Retorna: 'G' (si existe un ganador), 'E' (si es empate) o 'C' (si el juego continua)
         
         exists_winner = False;
@@ -60,13 +69,25 @@ class Board:
             else:
                 return 'C'
 
+
+    def change_current_player(self):
+        # Intercambiar el jugador actual, alternando entre jugador 1 y 2.
+
+        self.current_player = self.player_2 if self.current_player==self.player_1 else self.player_1
+
     
-    def start(self):        
-        self.render_instructions()
+    def start(self):
+        # Ejecutar el juego.
+
+        self.reset_board()        
         
         while True:
-            self.current_player.play_turn()
+            free_cells = { key:value for (key,value) in self.board_status.items() if value == ''}.keys()
+
+            cell = self.current_player.play_turn(free_cells)            
+            self.fill_cell(cell)
             self.render_board_status()
+
             result = self.check_board()
             if result == 'G':
                 print(f'Ganó el jugador {self.current_player}')
@@ -76,6 +97,6 @@ class Board:
                     print('Empate')
                     break
                 else:
-                    self.current_player = self.player_2 if self.current_player==self.player_1 else self.player_1
+                    self.change_current_player()
                     continue
             
--- modulos/ej2-tictactoe/main.py
+++ modulos/ej2-tictactoe/main.py
@@ -1,10 +1,16 @@
+import player
 from board import Board
 
-board = Board()
+player_x = player.Player('X')
+player_y = player.Player('Y')
+
+board = Board(player_x, player_y)
 
 while True:
-    iniciar_partida = input('Desea iniciar una partida? (S/N): ')
+    print('=' * 50)
+    iniciar_partida = input('Desea iniciar una partida? (S/N): ')    
     if iniciar_partida.upper() == 'S':
+        board.render_instructions()
         board.start()
     else:
         break
