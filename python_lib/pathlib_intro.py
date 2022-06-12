79ee57f76b1bc617fba5dd206237892c8d3b06db
Gabriel Villacis <gab_villacis@hotmail.com>
2022-06-12 18:09:15 -0500

    Update pathlib_intro.py

--- python_lib/pathlib_intro.py
+++ python_lib/pathlib_intro.py
@@ -1,50 +1,65 @@
+# %%
 # pathlib: https://docs.python.org/3/library/pathlib.html
 
 # importando la clase Path desde módulo pathlib
 from pathlib import Path
 
-# trabajando con rutas
-path1 = Path("C:\\Program Files\\Microsoft")
-print(path1)
+# %%
+# definiendo una ruta de directorio
+path_directorio = Path("C:\\Program Files\\Microsoft")
+print(path_directorio)
 
-path_carpeta_actual = Path()
-print(path_carpeta_actual)
+# %%
+# definiendo ruta al directorio actual
+path_directorio_actual = Path()
+print(path_directorio_actual)
+print(path_directorio_actual.absolute())
 
-path_archivo = Path("datasets") / Path("googleplaystore.csv")
+# %%
+# definiendo ruta relativa hacia un archivo
+path_archivo = Path("datasets/googleplaystore.csv")
 print(path_archivo)
 
-path_archivo = Path("datasets\\googleplaystore.csv")
-print(path_archivo)
-
+# %%
 # obteniendo el directorio del usuario del sistema operativo
 print(Path.home())
 
-path = Path("datasets/googleplaystore.csv")
+# %%
 # comprobar si la ruta existe
+path = Path("datasets/googleplaystore.csv")
 print(path.exists())
 
+# %%
 # comprobar si la ruta es un archivo
 print(path.is_file())
 
+# %%
 # comprobar si la ruta es un directorio
 print(path.is_dir())
 
+# %%
 # obteniendo el nombre del componente final de la ruta
-print(path.name)
+print(f'Ruta absoluta: {path.absolute()}')
+print(f'Componente final: {path.name}')
 
+# %%
 # obteniendo el nombre del componente final de la ruta sin el sufijo (extensión)
-print(path.stem)
+print(f'Ruta absoluta: {path.absolute()}')
+print(f'Componente final sin sufijo: {path.stem}')
 
+# %%
 # obteniendo el último sufijo (extensión) del componente final
-print(path.suffix)
+print(f'Ruta absoluta: {path.absolute()}')
+print(f'Sufijo: {path.suffix}')
 
+# %%
 # obteniendo el directorio padre donde se encuentra el archivo
-print(path.parent)
+print(f'Ruta absoluta: {path.absolute()}')
+print(f'Directorio padre: {path.parent}')
+print(f'Directorio padre absosulo: {path.parent.absolute()}')
 
-# obteniendo la ruta absoluta
-print(path.absolute())
-
-print(path)
-
+# %%
 # reemplazando solo el nombre del archivo dentro de la ruta
-print(path.with_name("archivo.txt"))
+print(f'Ruta absoluta: {path.absolute()}')
+print(path.with_name("googleplaystore_renombrado.csv"))
+# %%
