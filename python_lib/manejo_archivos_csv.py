f3f5f2aa52634b017cb58bc8066ffd6bd7115ad2
Gabriel Villacis <gab_villacis@hotmail.com>
2022-06-12 19:23:34 -0500

    Update manejo_archivos_csv.py

--- python_lib/manejo_archivos_csv.py
+++ python_lib/manejo_archivos_csv.py
@@ -1,19 +1,23 @@
+# %%
 # csv: https://docs.python.org/3/library/csv.html
-
+# importando módulo csv
 import csv
 
+# %%
 # leyendo archivo csv
 with open("datasets/googleplaystore.csv", encoding='utf-8') as csv_file:
     csv_reader = csv.reader(csv_file, delimiter = ',')
     for linea_csv in csv_reader:
         print(linea_csv)
 
+# %%
 # leyendo archivo csv en un diccionario
 with open("datasets/googleplaystore.csv", encoding='utf-8') as csv_file:
     csv_dict_reader = csv.DictReader(csv_file, delimiter=',')
     for dict_entry in csv_dict_reader:
         print(dict_entry)
 
+# %%
 # escribiendo archivo csv mediante la función writerow
 with open("datasets/estudiantes.csv", encoding='utf-8', mode='w+') as csv_file:
     writer = csv.writer(csv_file, delimiter = ',', lineterminator='\n')
@@ -23,6 +27,7 @@ with open("datasets/estudiantes.csv", encoding='utf-8', mode='w+') as csv_file:
     writer.writerow(["Hugo", "Torres", 20, "Quito"])
     writer.writerow(["Marcos", "Benavides", 18, "Quito"])
 
+# %%
 # escribiendo archivo csv mediante un diccionario
 with open("datasets/estudiantes.csv", encoding='utf-8', mode='w+') as csv_file:
     writer = csv.DictWriter(csv_file, fieldnames=['nombre', 'apellido', 'edad', 'ciudad'], lineterminator='\n')
@@ -34,9 +39,4 @@ with open("datasets/estudiantes.csv", encoding='utf-8', mode='w+') as csv_file:
     writer.writeheader()
     writer.writerows(lista_estudiantes)
     writer.writerow({'nombre': 'Juan Carlos', 'edad': 22})
-
-
-
-
-
-
+# %%
