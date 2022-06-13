d35be811a4667f5900d03511f5a4f572c960f6a6
Gabriel Villacis <gab_villacis@hotmail.com>
2022-06-12 19:08:21 -0500

    Update manejo_archivos.py

--- python_lib/manejo_directorios.py
+++ python_lib/manejo_directorios.py
@@ -1,35 +1,54 @@
+# %%
 # importando la clase Path desde módulo pathlib
 from pathlib import Path
 
-path = Path("D:\\carpeta_pruebas")
-
+# %%
+# verificando existencia de directorio
+path = Path('D:\\directorio_pruebas')
+print(path.absolute())
 print(path.exists())
 
+# %%
+# creando y borrando directorio
 if(not path.exists()):
     path.mkdir()
 else:
     path.rmdir()
     path.mkdir()
 
-print(path.absolute())
-# nuevo_path = path.rename("D:\\carpeta_pruebas_renombrado")
-# print(nuevo_path.absolute())
+# %%
+# renombrando directorio
+path_renombrado = path.rename("D:\\directorio_pruebas_renombrado")
+print(path_renombrado.absolute())
 
+# %%
+# apuntando al directorio actual
 directorio_actual = Path()
+print(directorio_actual)
+print(directorio_actual.absolute())
 
+# %%
 # listando el contenido del directorio
 for p in directorio_actual.iterdir():
     print(p)
 
+# %%
 # obteniendo el contenido del directorio como una lista
 paths = [p for p in directorio_actual.iterdir()]
 print(paths)
 
+# %%
+# obteniendo solo los subdirectorios
 paths = [p for p in directorio_actual.iterdir() if p.is_dir()]
 print(paths)
 
+# %%
+# obteniendo solo los archivos python
 archivos_python = [p for p in directorio_actual.glob("*.py")]
 print(archivos_python)
 
+# %%
+# obteniendo solo los archivos python de forma recursiva
 archivos_python = [p for p in directorio_actual.rglob("*.py")]
 print(archivos_python)
+# %%
